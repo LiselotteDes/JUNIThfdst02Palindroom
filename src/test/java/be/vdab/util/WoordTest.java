dc9f5063507651c43d91c2ac27fef323708468af
Liselotte <liselotte.des@gmail.com>
2018-02-08 13:40:27 +0100

    afgewerkt project

---
+++ src/test/java/be/vdab/util/WoordTest.java
@@ -0,0 +1,21 @@
+package be.vdab.util;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class WoordTest {
+	@Test
+	public void lepelIsEenPalindroom() {
+		Woord woord = new Woord("lepel");
+		assertTrue(woord.isPalindroom());
+	}
+	@Test
+	public void vorkIsGeenPalindroom() {
+		Woord woord = new Woord("vork");
+		assertFalse(woord.isPalindroom());
+	}
+	@Test
+	public void eenLegeStringIsEenPalindroom() {
+		Woord woord = new Woord("");
+		assertTrue(woord.isPalindroom());
+	}
+}
